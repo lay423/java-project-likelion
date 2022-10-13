f80531777461be50e67969a9fe046db20fef9f9d
lay423 <junpang486@gmail.com>
2022-10-13 13:56:47 +0900

    subdivision 추가하기 기능개선, 주석추가

--- src/main/java/com/line/parser/HospitalParser.java
+++ src/main/java/com/line/parser/HospitalParser.java
@@ -10,16 +10,17 @@ public class HospitalParser implements Parser<Hospital> {
         String subdivision = null;
         String[] subdivisionType = {"내과", "치과", "피부과", "성형외과", "외과", "가정의학과", "소아과", "한의원", };
 
+        //따옴표 제거
         for (int i = 0; i < splitted.length; i++) {
             splitted[i] = splitted[i].replace('\'', '\\');
             splitted[i] = splitted[i].replace("\"", "");
         }
-        //splitted[6] = String.valueOf(splitted[6].charAt(1));
 
-
+        //district 넣기
         String[] districtArr = splitted[1].split(" ");
         String district = districtArr[0] + " " + districtArr[1];
 
+        //subdivision 넣기
         for (int i = 0; i < subdivisionType.length; i++) {
             if(splitted[10].contains(subdivisionType[i])){
                 subdivision = subdivisionType[i];
