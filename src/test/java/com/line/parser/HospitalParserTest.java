fa04dc832d2c35df69ebb915a82e3f9b4b143615
lay423 <junpang486@gmail.com>
2022-10-13 14:50:23 +0900

    Test 추상화

--- src/test/java/com/line/parser/HospitalParserTest.java
+++ src/test/java/com/line/parser/HospitalParserTest.java
@@ -9,20 +9,38 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HospitalParserTest {
 
-    String line1 = "\"A1120837\",\"서울특별시 금천구 벚꽃로 286 삼성리더스타워 111~114호 (가산동)\",\"C\",\"의원\",\"G099\",\"응급의료기관 이외\",\"2\",\"외과: 상시진료 내과는 당분간 휴진\",\"서울시 송파구 문정동 장지동 법조단지 위례 가락동 가락시장역 위치 삼성서울병원 외래교수 출신 구강외과 전문의 진료 진료과목 - 임플란트 치조골 뼈이식 수술 매복 사랑니 발치 턱관절 악관절 질환의 치료 교정 치료 및 기타 보존 보철(크라운 브릿지 인레이) 신경치료\",\"방이역 1번출구 바로옆 굿모닝 신한증권 뒷건물\",\"가산기대찬의원\",\"02-6267-2580\",\"02-920-5374\",\"1930\",\"1930\",\"1930\",\"1930\",\"1930\",\"1500\",\"1500\",\"1500\",\"0900\",\"0900\",\"0900\",\"0900\",\"0900\",\"0900\",\"1000\",\"1000\",\"085\",\"11\",\"126.88412249700781\",\"37.4803938036867\",\"2022-04-07 14:55:00.0\"";
+    String line1 = "\"A1120837\",\"서울특별시 금천구 벚꽃로 286 삼성리더스타워 111~114호 (가산동)\",\"C\",\"의원\",\"G099\",\"응급의료기관 이외\",\"2\",\"외과: 상시진료 내과는 당분간 휴진\",\"서울수 출 신경치료\",\"방이역 1번출구 바로옆 굿모닝 신한증권 뒷건물\",\"가산기대찬의원\",\"02-6267-2580\",\"02-920-5374\",\"1930\",\"1930\",\"1930\",\"1930\",\"1930\",\"1500\",\"1500\",\"1500\",\"0900\",\"0900\",\"0900\",\"0900\",\"0900\",\"0900\",\"1000\",\"1000\",\"085\",\"11\",\"126.88412249700781\",\"37.4803938036867\",\"2022-04-07 14:55:00.0\"";
+    String line2 = "\"A1117873\",\"서울특별시 관악구 신원로 38 5층 (신림동 청암빌딩)\",\"N\",\"의원\",\"G099\",\"응급의료기관 이외\",2,\"외과: 상시진료 내과는 당분간 휴진\",\"서울시 목 인레이) 신경치료\",\"방이역 1번출구 바로옆 굿모닝 신한증권 뒷건물\",\"가로수치과의원\",\"치과\"";
 
+    private void assertCategories(Hospital hospital, String id, String address, int emergency_room, String category, String name){
+
+        //id 테스트
+        assertEquals(id, hospital.getId());
+
+        //주소
+        assertEquals(address, hospital.getAddress());
+
+        //Emergency_room
+        assertEquals(emergency_room, hospital.getEmergency_room());
+
+        //Category
+        assertEquals(category, hospital.getCategory());
+
+        //Name
+        assertEquals(name, hospital.getName());
+    }
     @Test
     @DisplayName("id, 주소를 잘 파싱하는지 테스트")
     void name(){
         HospitalParser hospitalParser = new HospitalParser();
-        Hospital hospital = hospitalParser.parse(this.line1);
-        assertEquals("A1120837", hospital.getId());
-        assertEquals(
-                "서울특별시 금천구 벚꽃로 286 삼성리더스타워 111~114호 (가산동)"
-                , hospital.getAddress());
-        assertEquals(2, hospital.getEmergency_room());
-        assertEquals("C", hospital.getCategory());
-        assertEquals("가산기대찬의원", hospital.getName());
+
+        assertCategories(hospitalParser.parse(this.line1),
+        "A1120837",
+        "서울특별시 금천구 벚꽃로 286 삼성리더스타워 111~114호 (가산동)",
+        2,
+        "C",
+        "가산기대찬의원");
+
     }
 
 }
