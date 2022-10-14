76927cf4c6bd2d56d23050df81b5dc3dec117ea4
lay423 <junpang486@gmail.com>
2022-10-14 10:14:15 +0900

    i를 j로 바꾸기

--- src/main/java/java1014/sort/InsertionSort2.java
+++ src/main/java/java1014/sort/InsertionSort2.java
@@ -3,15 +3,15 @@ package java1014.sort;
 import java.util.Arrays;
 
 public class InsertionSort2 {
-    public int[] sort(int[] arr) {
+    public int[] sort(int[] arr, int n) {
         int targetVal;
         int targetIdx;
-        int i=1;
+        int j = n;
 
-        if (arr[i] < arr[i - 1]) {
-            int tmp = arr[i-1];
-            arr[i-1] = arr[i];
-            arr[i] = tmp;
+        if (arr[j] < arr[j - 1]) {
+            int tmp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = tmp;
         }
 
         return arr;
@@ -23,7 +23,8 @@ public class InsertionSort2 {
         int[] arr = {8, 5, 6, 2, 4};
         System.out.println(Arrays.toString(arr));
 
-        int[] result = insertionSort.sort(arr);
+        arr = new int[]{5, 8, 6, 2, 4};
+        int[] result = insertionSort.sort(arr, 2);
         System.out.println(Arrays.toString(result));
     /*
         [8, 5, 6, 2, 4]
