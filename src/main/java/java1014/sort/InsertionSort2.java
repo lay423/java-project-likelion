41eada7d22adefc40f89e377680d06a015c62162
lay423 <junpang486@gmail.com>
2022-10-14 10:24:55 +0900

    삽입정렬

--- src/main/java/java1014/sort/InsertionSort2.java
+++ src/main/java/java1014/sort/InsertionSort2.java
@@ -3,15 +3,20 @@ package java1014.sort;
 import java.util.Arrays;
 
 public class InsertionSort2 {
-    public int[] sort(int[] arr, int n) {
+    public int[] sort(int[] arr) {
         int targetVal;
         int targetIdx;
-        int j = n;
-
-        if (arr[j] < arr[j - 1]) {
-            int tmp = arr[j - 1];
-            arr[j - 1] = arr[j];
-            arr[j] = tmp;
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int tmp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+            //회전과정 출력
+            System.out.print(i + "회전 : ");
+            System.out.println(Arrays.toString(arr));
         }
 
         return arr;
@@ -23,12 +28,15 @@ public class InsertionSort2 {
         int[] arr = {8, 5, 6, 2, 4};
         System.out.println(Arrays.toString(arr));
 
-        arr = new int[]{5, 8, 6, 2, 4};
-        int[] result = insertionSort.sort(arr, 2);
+        int[] result = insertionSort.sort(arr);
         System.out.println(Arrays.toString(result));
     /*
         [8, 5, 6, 2, 4]
-        [5, 8, 6, 2, 4]
-    */
+        1회전 : [5, 8, 6, 2, 4]
+        2회전 : [5, 6, 8, 2, 4]
+        3회전 : [2, 5, 6, 8, 4]
+        4회전 : [2, 4, 5, 6, 8]
+        [2, 4, 5, 6, 8]
+     */
     }
 }
