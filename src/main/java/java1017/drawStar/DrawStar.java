05c72732e74187b5864d7a04cdf707a354fd34a2
lay423 <junpang486@gmail.com>
2022-10-17 09:48:44 +0900

    피라미드 별찍기

--- src/main/java/java1017/drawStar/DrawStar.java
+++ src/main/java/java1017/drawStar/DrawStar.java
@@ -11,10 +11,22 @@ public class DrawStar {
         this.c = c;
     }
 
-    public void draw(int n) {
+    public void drawRightTriangle(int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
-                System.out.print(c+" ");
+                System.out.print(c + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void drawPyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = n - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = i * 2 - 1; j > 0; j--) {
+                System.out.print(c);
             }
             System.out.println();
         }
--- src/main/java/java1017/drawStar/Main.java
+++ src/main/java/java1017/drawStar/Main.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        DrawStar star = new DrawStar('#');
+        DrawStar star = new DrawStar('*');
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        star.draw(num);
+        star.drawPyramid(num);
 
     }
 }
