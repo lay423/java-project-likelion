9380cc69187cccea1e5ab4f1aaf11a7930009022
lay423 <junpang486@gmail.com>
2022-10-17 09:07:09 +0900

    별찍기

---
+++ src/main/java/java1017/drawStar/Main.java
@@ -0,0 +1,10 @@
+package java1017.drawStar;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+    }
+}
