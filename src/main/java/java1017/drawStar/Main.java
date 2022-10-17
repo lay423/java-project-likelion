bda564d871ead4fc1cf955cd4624209b2fa404e7
lay423 <junpang486@gmail.com>
2022-10-17 09:17:25 +0900

    별찍기 for문

--- src/main/java/java1017/drawStar/Main.java
+++ src/main/java/java1017/drawStar/Main.java
@@ -2,9 +2,11 @@ package java1017.drawStar;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("* *");
-        System.out.println("* * *");
-        System.out.println("* * * *");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
