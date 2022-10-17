93f749a7b1305a40e8cfeadddf5918ee44fce3ef
lay423 <junpang486@gmail.com>
2022-10-17 09:20:09 +0900

    별찍기 for문, scanner

--- src/main/java/java1017/drawStar/Main.java
+++ src/main/java/java1017/drawStar/Main.java
@@ -1,8 +1,12 @@
 package java1017.drawStar;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 4; i++) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 0; i < num; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print("* ");
             }
