ad2c15cfc77d2b8d893bbf0638bb01064facbbc1
lay423 <junpang486@gmail.com>
2022-10-18 15:30:58 +0900

    interface 도입

---
+++ src/main/java/com/dbexercise/dao/ConnectionMaker.java
@@ -0,0 +1,8 @@
+package com.dbexercise.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface ConnectionMaker {
+    Connection makeConnection() throws SQLException;
+}
