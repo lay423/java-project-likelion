3407eb642934b89f683e996e43a2c916a5a9e2f2
lay423 <junpang486@gmail.com>
2022-10-18 10:18:38 +0900

    line-parser에 insert, select 만들기

---
+++ src/main/java/com/dbexercise/domain/User.java
@@ -0,0 +1,29 @@
+package com.dbexercise.domain;
+
+public class User {
+    private String id;
+    private String name;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
---
+++ src/main/java/com/line/dao/HospitalDao.java
@@ -0,0 +1,71 @@
+package com.line.dao;
+
+import com.line.domain.Hospital;
+
+import java.sql.*;
+import java.util.Map;
+
+public class HospitalDao {
+
+    public void add() throws ClassNotFoundException, SQLException {
+        Map<String, String> env = System.getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbUser = env.get("DB_USER");
+        String dbPassword = env.get("DB_PASSWORD");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO `hospital`.`seoul_hospital`\n" +
+                "(`id`,\n" +
+                "`address`,\n" +
+                "`district`,\n" +
+                "`category`,\n" +
+                "`emergency_room`,\n" +
+                "`name`,\n" +
+                "`subdivision`)\n" +
+                "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?");
+        ps.setString(1, "3");
+        ps.setString(2, "강남구청 집돌이에");
+        ps.setString(3, "서울특별시 강남구");
+        ps.setString(4, "N");
+        ps.setString(5, "2");
+        ps.setString(6, "병원이름병원");
+        ps.setString(7, "치과");
+
+        ps.executeUpdate();
+        ps.close();
+        conn.close();
+
+    }
+
+    public Hospital get() throws ClassNotFoundException, SQLException {
+        Map<String, String> env = System.getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbUser = env.get("DB_USER");
+        String dbPassword = env.get("DB_PASSWORD");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
+        PreparedStatement ps = conn.prepareStatement("SELECT id, address, district" +
+                "category, emergency_room, name, subdivision FROM `hospital`.`seoul_hospital`" +
+                "WHERE id = ?");
+        ps.setString(1, "1");
+
+        ResultSet rs = ps.executeQuery();
+        Hospital hospital = new Hospital(
+                rs.getString("id"),
+                rs.getString("address"),
+                rs.getString("district"),
+                rs.getString("category"),
+                rs.getInt("emergency_room"),
+                rs.getString("name"),
+                rs.getString("subdivision")
+        );
+        rs.close();
+        ps.close();
+        conn.close();
+
+        return hospital;
+    }
+
+}
