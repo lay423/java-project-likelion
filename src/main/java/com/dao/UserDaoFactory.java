6f25cbc9c3a9d0f89abbda5262f5c0eb721931d8
lay423 <junpang486@gmail.com>
2022-10-19 13:39:54 +0900

    spring 적용

--- src/main/java/com/dao/UserDaoFactory.java
+++ src/main/java/com/dao/UserDaoFactory.java
@@ -1,13 +1,19 @@
 package com.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UserDaoFactory {
 
+    @Bean
     public UserDao awsUserDao(){    // 날개 5개 선풍기
         AWSConnectionMaker awsConnectionMaker = new AWSConnectionMaker();
         UserDao userDao = new UserDao(awsConnectionMaker);
         return userDao;
     }
 
+    @Bean
     public UserDao localUserDao() {
         UserDao userDao = new UserDao(new LocalConnectionMaker());
         return userDao;
--- src/test/java/com/dbexercise2/dao/UserDaoTest.java
+++ src/test/java/com/dbexercise2/dao/UserDaoTest.java
@@ -6,21 +6,31 @@ import com.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
 class UserDaoTest {
+
+    @Autowired
+    ApplicationContext context;
 
     @Test
     @DisplayName("insert와 select가 잘 작동되는지 test")
     public void insertAndSelect() throws SQLException {
 
         //UserDao userDao = new UserDao();
-        UserDao userDao = new UserDaoFactory().awsUserDao();
-        User user = new User("11", "king1", "11223");
+        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
+        User user = new User("12", "king1", "11223");
         userDao.insert(user);
 
-        User selectUser = userDao.select("11");
-        Assertions.assertEquals("king", selectUser.getName());
+        User selectUser = userDao.select("12");
+        Assertions.assertEquals("king1", selectUser.getName());
     }
 }
