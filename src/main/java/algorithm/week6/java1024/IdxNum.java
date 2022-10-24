a7c7ff2b5d8fbbee2b4fd5765ba1705145a1fead
lay423 <junpang486@gmail.com>
2022-10-24 10:18:16 +0900

    우선순위 큐 이용

--- src/main/java/algorithm/week6/java1024/IdxNum.java
+++ src/main/java/algorithm/week6/java1024/IdxNum.java
@@ -1,9 +1,10 @@
 package algorithm.week6.java1024;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class IdxNum {
-
+    int[] arr = {1, 5, 2, 6, 3, 7, 4};
     public int[] solution(int[] array, int[][] commands){
         int[] answer= new int[commands.length];
 
@@ -16,12 +17,38 @@ public class IdxNum {
         return answer;
     }
 
+    public int getKthNum(int[] command) {
+        int frIdx = command[0];
+        int toIdx = command[1];
+        int nth = command[2];
+
+        int result = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = frIdx - 1; i < toIdx; i++) {
+            pq.add(arr[i]);
+        }
+
+        for (int i = 0; i < nth; i++) {
+            result = pq.poll();
+        }
+        return result;
+    }
+
+    public int[] solution2(int[] arr, int[][] commands) {
+        this.arr = arr;
+        int[] answer = new int[commands.length];
+        for (int i = 0; i < commands.length; i++) {
+            answer[i] = getKthNum(commands[i]);
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 5, 2, 6, 3, 7, 4};
 
         int[][] commands = new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
         IdxNum idxNum = new IdxNum();
-        arr = idxNum.solution(arr, commands);
+        arr = idxNum.solution2(arr, commands);
         System.out.println(Arrays.toString(arr));
     }
 }
