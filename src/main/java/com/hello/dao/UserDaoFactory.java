e05438ecbcc1917237526952dcba540a6f1acfff
lay423 <junpang486@gmail.com>
2022-10-24 11:42:48 +0900

    dataSource

--- src/main/java/com/hello/dao/UserDaoFactory.java
+++ src/main/java/com/hello/dao/UserDaoFactory.java
@@ -2,12 +2,23 @@ package com.hello.dao;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Map;
 
 @Configuration
 public class UserDaoFactory {
 
     @Bean
-    public UserDao awsUserDao(){    // 날개 5개 선풍기
+    UserDao awsUserDao(){
+        return new UserDao(dataSource());
+    }
+
+    @Bean
+    public UserDao awsUserDao1(){    // 날개 5개 선풍기
         AWSConnectionMaker awsConnectionMaker = new AWSConnectionMaker();
         UserDao userDao = new UserDao(awsConnectionMaker);
         return userDao;
@@ -17,5 +28,16 @@ public class UserDaoFactory {
     public UserDao localUserDao() {
         UserDao userDao = new UserDao(new LocalConnectionMaker());
         return userDao;
+    }
+
+    @Bean
+    DataSource dataSource() {
+        Map<String, String> env = System.getenv();
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
+        dataSource.setUrl(env.get("DB_HOST"));
+        dataSource.setUsername(env.get("DB_USER"));
+        dataSource.setPassword(env.get("DB_PASSWORD"));
+        return dataSource;
     }
 }
