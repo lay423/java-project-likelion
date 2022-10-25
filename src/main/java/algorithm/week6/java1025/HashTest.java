b14dd6cf3582d071597bd8d02b471e3d6bd95e1b
lay423 <junpang486@gmail.com>
2022-10-25 10:07:31 +0900

    hash(), insert(), search() 실습

---
+++ src/main/java/algorithm/week6/java1025/HashTest.java
@@ -0,0 +1,50 @@
+package algorithm.week6.java1025;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashTest {
+
+    private int size = 1000;
+    private int[] table = new int[size];
+    public HashTest() {
+    }
+
+    public HashTest(int size){
+        this.size = size;
+        this.table = new int[size];
+    }
+
+    public void insert(String key, Integer value) {
+        int hashCode = hash(key);
+        this.table[hashCode] = value;
+        System.out.println(key+" " +hashCode+"방에 저장 완료");
+    }
+
+    public int search(String key) {
+        return this.table[hash(key)];
+    }
+    public int hash(String str) {
+        int ascii = 0;
+        for (int i = 0; i < str.length(); i++) {
+            ascii += str.charAt(i);
+        }
+        return ascii % 1000;
+    }
+
+    public static void main(String[] args) {
+        HashTest ht = new HashTest();
+        String[] names = new String[]{"DongyeonKang",
+                "SubinKang", "KwanwunKo", "HyunseokKo", "KyoungdukKoo", "YeonjiGu", "SoyeonKown", "OhsukKwon", "GunwooKim", "KiheonKim", "NayeongKim", "DohyeonKim", "MinkyoungKim", "MinjiKim", "SanghoKim", "SolbaeKim", "YejinKim", "EungjunKim", "JaegeunKim", "JeonghyeonKim", "JunhoKim", "JisuKim", "kimjinah", "HaneulKim", "HeejungKim", "KimoonPark", "EunbinPark", "JeongHoonPark", "JeminPark", "TaegeunPark", "JiwonBae", "SeunggeunBaek", "JihwanByeon", "HeungseopByeon", "JeongHeeSeo", "TaegeonSeo", "SeeYunSeok", "SuyeonSeong", "SeyoelSon", "MinjiSong", "JinwooSong", "hyunboSim", "SominAhn", "JiyoungAhn", "ChangbumAn", "SoonminEom",
+
+                "HyeongsangOh", "SuinWoo", "JuwanWoo", "InkyuYoon", "GahyunLee", "DaonLee", "DohyunLee", "SanghunLee", "SujinLee", "AjinLee", "YeonJae", "HyeonjuLee", "HakjunYim", "SeoyunJang", "SeohyeonJang", "JinseonJang", "SujinJeon", "SeunghwanJeon", "DaehwanJung", "JaeHyunJeung", "HeejunJeong", "GukhyeonCho", "MunjuJo", "YejiJo", "ChanminJu", "MinjunChoi", "SujeongChoi", "SeunghoChoi", "AyeongChoi", "GeonjooHan", "JinhyuckHeo", "MinwooHwang", "SieunHwang",
+                "JunhaHwang"};
+        for (int i = 0; i < names.length; i++) {
+            ht.insert(names[i], ht.hash(names[i]));
+
+        }
+        System.out.println(ht.search("DongyeonKang"));
+        System.out.println(ht.search("JiyoungAhn"));
+
+    }
+}
