4e00216caa62347add2d0d36b890b7a81ee6840a
lay423 <junpang486@gmail.com>
2022-10-27 10:27:28 +0900

    프로그래머스 완주하지 못한 선수

--- src/main/java/algorithm/week6/java1027/Marathoner.java
+++ src/main/java/algorithm/week6/java1027/Marathoner.java
@@ -1,14 +1,29 @@
 package algorithm.week6.java1027;
 
-import algorithm.week6.java1026.HashTable;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Marathoner {
 
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+        Map<String, Integer> memo = new HashMap<>();
+        for (int i = 0; i < participant.length; i++) {
+            String key = participant[i];
+            if (memo.get(key) == null) {
+                memo.put(key, 0);
+            }
+            memo.put(key, memo.get(key)+1);
+        }
+        for (int i = 0; i < completion.length; i++) {
+            String key = completion[i];
+            memo.put(key, memo.get(key)-1);
+        }
+        for(String key : memo.keySet()){
+            if (memo.get(key)==1) {
+                return key;
+            }
+        }
 
         return answer;
     }
@@ -17,7 +32,11 @@ public class Marathoner {
         Marathoner mt = new Marathoner();
         String[] arr = new String[]{"leo", "kiki", "eden"};
         String[] arr1 = new String[]{"kiki", "eden"};
+        String[] arr2 = new String[]{"mislav", "stanko", "mislav", "ana"};
+        String[] arr3 = new String[]{"stanko", "ana", "mislav"};
 
         System.out.println(mt.solution(arr, arr1));
+        System.out.println(mt.solution(arr2, arr3));
+
     }
 }
