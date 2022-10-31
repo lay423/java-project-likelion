6820016958908e38fdd919fb3e1442e222527f4d
lay423 <junpang486@gmail.com>
2022-10-31 09:41:14 +0900

    프로그래머스 모의고사 | 몇문제 맞췄는지 출력

--- src/main/java/algorithm/week7/java1031/MockExam.java
+++ src/main/java/algorithm/week7/java1031/MockExam.java
@@ -2,31 +2,42 @@ package algorithm.week7.java1031;
 
 public class MockExam {
     public int[] solution(int[] answer) {
-        int[] a = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+        int[] std1 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+        int[] std2 = {2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5};
+        int[] std3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         int firstStdAnswerCnt=0;
+        int secondStdAnswerCnt=0;
+        int thirdStdAnswerCnt=0;
         for (int i = 0; i < answer.length; i++) {
-            if (answer[i] % a[i]==0) {
+            if (answer[i] == std1[i]) {
                 firstStdAnswerCnt++;
             }
+            if (answer[i] == std2[i]) {
+                secondStdAnswerCnt++;
+            }
+            if (answer[i] == std3[i]) {
+                thirdStdAnswerCnt++;
+            }
         }
-
-
-        System.out.println(firstStdAnswerCnt);
+        System.out.printf("수포자 1은 %d개 문제를 맞혔습니다.\n", firstStdAnswerCnt);
+        System.out.printf("수포자 2은 %d개 문제를 맞혔습니다.\n", secondStdAnswerCnt);
+        System.out.printf("수포자 3은 %d개 문제를 맞혔습니다.\n", thirdStdAnswerCnt);
         return answer;
     }
 
     public static void main(String[] args) {
-        int[] a = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
-        int[] a2 = {2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5};
-        int[] a3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        // 1번 anwer --> 5
-        // 2번 anwer --> 0
-        // 1번 anwer --> 5
-        int[] answer = {1, 3, 2, 4, 2};
-        int[] answer2 = {1, 2, 3, 4, 5};
+        int[] answer1 = {1, 2, 3, 4, 5};
+        int[] answer2 = {1, 3, 2, 4, 2};
         MockExam mExam = new MockExam();
+        mExam.solution(answer1);
         mExam.solution(answer2);
-        mExam.solution(answer);
-
+    /*
+        수포자 1은 5개 문제를 맞혔습니다.
+        수포자 2은 0개 문제를 맞혔습니다.
+        수포자 3은 0개 문제를 맞혔습니다.
+        수포자 1은 2개 문제를 맞혔습니다.
+        수포자 2은 2개 문제를 맞혔습니다.
+        수포자 3은 2개 문제를 맞혔습니다.
+     */
     }
 }
