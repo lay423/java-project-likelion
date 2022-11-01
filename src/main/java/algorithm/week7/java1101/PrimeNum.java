bbf1982f789f5d97499c027040928b706c8078d2
lay423 <junpang486@gmail.com>
2022-11-01 09:32:28 +0900

    프로그래머스 소수 찾기 | 2단계로 찾기

--- src/main/java/algorithm/week7/java1101/PrimeNum.java
+++ src/main/java/algorithm/week7/java1101/PrimeNum.java
@@ -18,7 +18,7 @@ public class PrimeNum {
 
     public boolean isPrimeNum(int n) {
         int cntDivisor=0;
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i < n/2; i++) {
             if (n % i == 0) {
                 cntDivisor++;
             }
