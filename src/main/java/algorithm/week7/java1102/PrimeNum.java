658f7c51f03781bcd7593400ad74172afda387d7
lay423 <junpang486@gmail.com>
2022-11-02 09:46:48 +0900

    프로그래머스 소수 찾기 | 에라토스테네스의 채 적용

--- src/main/java/algorithm/week7/java1102/PrimeNum.java
+++ src/main/java/algorithm/week7/java1102/PrimeNum.java
@@ -5,27 +5,25 @@ import java.util.List;
 
 public class PrimeNum {
 
+    public int solution(int n) {
+        List<Integer> primeNums = new ArrayList<>();
+        for (int i = 2; i <= n; i++) {
+            primeNums.add(i);
+        }
+        for (int i = 2; i < n; i++) {
+            if(!(primeNums.contains(i)))
+                continue;
+            for (int j = 1; j < primeNums.size(); j++) {
+                if (primeNums.get(j) % i == 0 && primeNums.get(j)!=i) {
+                    primeNums.remove(j);
+                }
+            }
+        }
+        return primeNums.size();
+    }
+
     public static void main(String[] args) {
-        List<Integer> integers = new ArrayList<>();
-
-        for (int i = 2; i < 50; i++) {
-            integers.add(i);
-        }
-
-        for (int i = 1; i < integers.size(); i++) {
-            if (integers.get(i) % 2 == 0) {
-                integers.remove(i);
-            }
-        }
-        System.out.println(integers.size());
-
-        for (int i = 2; i < integers.size(); i++) {
-            if (integers.get(i) % 3 == 0) {
-                integers.remove(i);
-            }
-        }
-
-        System.out.println(integers.size());
-
+        PrimeNum pn = new PrimeNum();
+        System.out.println(pn.solution(100));
     }
 }
