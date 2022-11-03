1f257e3dd72780749eac50cdcbec12592a32f7f6
lay423 <junpang486@gmail.com>
2022-11-03 09:18:17 +0900

    프로그래머스 소수 찾기 | multipleOf

---
+++ src/main/java/algorithm/week7/java1103/PrimeNum.java
@@ -0,0 +1,9 @@
+package algorithm.week7.java1103;
+
+public class PrimeNum {
+    public static void main(String[] args) {
+        for (int i = 2; i < 6; i++) {
+            System.out.println(i+" "+2*(i-1));
+        }
+    }
+}
