165133a286ffb935533b4507ca78af541363b361
lay423 <junpang486@gmail.com>
2022-11-07 09:23:48 +0900

    프로그래머스 하샤드 수 | 제출, 통과

--- src/main/java/algorithm/week8/java1107/Harshad.java
+++ src/main/java/algorithm/week8/java1107/Harshad.java
@@ -3,25 +3,29 @@ package algorithm.week8.java1107;
 import java.util.Arrays;
 
 public class Harshad {
-    //자릿수 합 구하기
-    //나누어떨어지는지
-    public static void main(String[] args) {
-        int num=13456;
+
+    public boolean solution(int x) {
+        boolean answer = true;
+        int num = x;
         int divNum=10000;
         int[] digitNums = new int[]{0,0,0,0,0};
+        int cnt=0;
 
         for (int i = 0; i < digitNums.length; i++) {
             if (num / divNum != 0) {
                 digitNums[i]=num/divNum;
+                cnt += digitNums[i];
             }
             num%=divNum;
             divNum/=10;
         }
-        System.out.println(Arrays.toString(digitNums));
-        int cnt=0;
-        for (int digitNum : digitNums) {
-            cnt+=digitNum;
+        if (x % cnt == 0) {
+            return true;
         }
-        System.out.println(cnt);
+        return false;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
