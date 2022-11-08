599ee845624a05ba2202cc027d269ce676834a91
lay423 <junpang486@gmail.com>
2022-11-08 10:52:14 +0900

    이진탐색 해결

--- src/main/java/algorithm/week8/java1108/BinarySearch.java
+++ src/main/java/algorithm/week8/java1108/BinarySearch.java
@@ -5,15 +5,15 @@ public class BinarySearch {
     public String search(int[] arr, int target) {
         int startIdx = 0;
         int endIdx = arr.length - 1;
+        int midIdx=(endIdx - startIdx)/2;
         while (startIdx - endIdx != 0) {
-            int midIdx = (endIdx - startIdx)/2;
+            midIdx = (endIdx + startIdx)/2;
             System.out.println(midIdx);
-            if (arr[midIdx] > target) endIdx = midIdx;
-            else if (arr[midIdx] < target) startIdx = midIdx;
+            if (arr[midIdx] > target) endIdx = midIdx-1;
+            else if (arr[midIdx] < target) startIdx = midIdx+1;
             else {
                 return "else문 도달 midIdx 값: " + arr[midIdx];
             }
-            System.out.printf("%d %d %d", startIdx, midIdx, endIdx);
 
         }
         if (arr[startIdx] == target) {
@@ -25,8 +25,8 @@ public class BinarySearch {
 
     public static void main(String[] args) {
         BinarySearch bs = new BinarySearch();
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-        int target = 8;
+        int[] arr = {1, 2, 3, 4,  6, 7, 8, 9, 10, 11};
+        int target = 5;
         System.out.println(bs.search(arr, target));
     }
 }
