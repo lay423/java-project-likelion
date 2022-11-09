d94e54aad60ca2e2f9f6b87205a3fca6c0474c1c
lay423 <junpang486@gmail.com>
2022-11-09 09:39:28 +0900

    선택정렬 완료

--- src/main/java/algorithm/week8/java1109/SelectionSort.java
+++ src/main/java/algorithm/week8/java1109/SelectionSort.java
@@ -6,28 +6,17 @@ public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
 
-        int i=0;
-        for (int j = i; j < arr.length; j++) {
+
+        for (int i = 0; i < arr.length; i++) {
             int minIdx = i;
-            if (arr[minIdx] > arr[j]) {
-                minIdx = j;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[minIdx] > arr[j]) minIdx = j;
             }
             int tmp=arr[minIdx];
             arr[minIdx] = arr[i];
             arr[i] = tmp;
-        }
-        System.out.println((i+1)+"번째 "+Arrays.toString(arr));
-        i=1;
-        for (int j = i; j < arr.length; j++) {
-            if (arr[i] > arr[j]) {
-                int tmp=arr[i];
-                arr[i] = arr[j];
-                arr[j] = tmp;
-            }
-        }
-        System.out.println((i+1)+"번째 "+Arrays.toString(arr));
-        i=2;
 
-        i=3;
+            System.out.println((i+1)+"번째 "+Arrays.toString(arr));
+        }
     }
 }
