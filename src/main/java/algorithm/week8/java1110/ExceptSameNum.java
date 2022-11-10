f0e50615552bb3307297a2ab4fd92980715d41d5
lay423 <junpang486@gmail.com>
2022-11-10 09:31:13 +0900

    프로그래머스 같은 숫자는 싫어 | 스택안쓰고 풀기, 스택쓰고 풀기

--- src/main/java/algorithm/week8/java1110/ExceptSameNum.java
+++ src/main/java/algorithm/week8/java1110/ExceptSameNum.java
@@ -1,6 +1,8 @@
 package algorithm.week8.java1110;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class ExceptSameNum {
@@ -20,9 +22,25 @@ public class ExceptSameNum {
         return answer;
     }
 
+    public int[] solution2(int[] arr) {
+        List<Integer> numList = new ArrayList<>();
+        numList.add(arr[0]);
+        for (int i=1; i<arr.length;i++) {
+            if (numList.get(numList.size()-1) != arr[i]) {
+                numList.add(arr[i]);
+            }
+        }
+
+        int[] answer = new int[numList.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = numList.get(i);
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
         ExceptSameNum esn = new ExceptSameNum();
         int[] arr = new int[]{1, 1, 3, 3, 0, 1, 1};
-        System.out.println(Arrays.toString(esn.solution(arr)));
+        System.out.println(Arrays.toString(esn.solution2(arr)));
     }
 }
