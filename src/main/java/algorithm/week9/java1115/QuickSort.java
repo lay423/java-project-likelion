91960a9e56a6d1ad83a88a3c7893d715b70abd2f
lay423 <junpang486@gmail.com>
2022-11-15 10:33:28 +0900

    퀵정렬 | sort() 오류 해결중

--- src/main/java/algorithm/week9/java1115/QuickSort.java
+++ src/main/java/algorithm/week9/java1115/QuickSort.java
@@ -4,27 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class QuickSort {
-    public static void main(String[] args) {
 
-        int[] arr = new int[]{20, 18, 5, 19, 40, 25, 5, 50};
-        int mid = arr[arr.length / 2];
+    public List<Integer> merge(List<Integer> left, List<Integer> mid, List<Integer> right) {
+        List<Integer> answer = new ArrayList<>();
+        answer.addAll(left);
+        answer.addAll(mid);
+        answer.addAll(right);
+        return answer;
+
+    }
+    public List<Integer> sort(List<Integer> arr) {
         List<Integer> firstList = new ArrayList<>();
         List<Integer> midList = new ArrayList<>();
         List<Integer> lastList = new ArrayList<>();
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < mid) {
-                firstList.add(arr[i]);
-            } else if (arr[i] > mid) {
-                lastList.add(arr[i]);
+        int mid = arr.get(arr.size() / 2);
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) < mid) {
+                firstList.add(arr.get(i));
+            } else if (arr.get(i) > mid) {
+                lastList.add(arr.get(i));
             }else {
-                midList.add(arr[i]);
+                midList.add(arr.get(i));
             }
         }
+        return merge(sort(firstList), sort(midList), sort(lastList));
+    }
 
-        System.out.println(firstList.toString());
-        System.out.println(midList.toString());
-        System.out.println(lastList.toString());
-
+    public static void main(String[] args) {
+        QuickSort qs = new QuickSort();
+        int[] arr = new int[]{20, 18, 5, 19, 40, 25, 5, 50};
+        List<Integer> beforeList = new ArrayList<>();
+        for (int i : arr) {
+            beforeList.add(i);
+        }
+        List<Integer> arrList = qs.sort(beforeList);
+        System.out.println(arrList);
     }
 }
