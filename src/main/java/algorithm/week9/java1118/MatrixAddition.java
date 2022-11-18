77da2f061a5cc2bddf8f3e15f0e8b59a15f1c6c1
lay423 <junpang486@gmail.com>
2022-11-18 09:50:59 +0900

    행렬의 덧셈 | 성공

---
+++ src/main/java/algorithm/week9/java1118/MatrixAddition.java
@@ -0,0 +1,23 @@
+package algorithm.week9.java1118;
+
+import org.junit.jupiter.api.Assertions;
+
+public class MatrixAddition {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = {};
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                arr1[i][j] += arr2[i][j];
+            }
+        }
+        return arr1;
+    }
+
+    public static void main(String[] args) {
+        MatrixAddition ma = new MatrixAddition();
+        int[][] arr1 = new int[][]{{1, 2}, {2, 3}};
+        int[][] arr2 = new int[][]{{3, 4}, {5, 6}};
+        Assertions.assertEquals(new int[][]{{4, 6}, {7, 9}}, ma.solution(arr1, arr2));
+
+    }
+}
