6446be85dc4fe00b3452dad9d5f9113a5d120944
lay423 <junpang486@gmail.com>
2022-11-22 09:24:13 +0900

    [Update] 탈출조건, 1씩 커지는 로직 추가

--- src/main/java/algorithm/week10/java1122/Print1to100.java
+++ src/main/java/algorithm/week10/java1122/Print1to100.java
@@ -6,10 +6,10 @@ package algorithm.week10.java1122;
 public class Print1to100 {
     public static void print1To100 (int n){
         if(n>100)
+            return;
 
         System.out.println(n);
-        print1To100(n);
-        n++;
+        print1To100(n+1);
     }
 
     public static void main(String[] args) {
