203481f5a4a07a0ea1079d7f635a6ea6fbb913ad
lay423 <junpang486@gmail.com>
2022-11-22 10:12:58 +0900

    [Update] 리스트에 있는 모든 값 더하기 | 재귀사용

--- src/main/java/algorithm/week10/java1122/Print1to100.java
+++ src/main/java/algorithm/week10/java1122/Print1to100.java
@@ -1,7 +1,8 @@
 package algorithm.week10.java1122;
 
 
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Print1to100 {
     public static void print1To100 (int n){
@@ -12,8 +13,25 @@ public class Print1to100 {
         print1To100(n+1);
     }
 
+    public static int sum(List<Integer> nums) {
+        if(nums.isEmpty())
+            return 0;
+        if(nums.size()==1)
+            return nums.get(0);
+        nums.add(nums.get(0) + nums.get(1));
+        nums.remove(0);
+        nums.remove(0);
+        return sum(nums);
+
+    }
+
     public static void main(String[] args) {
-        print1To100(1);
+        List<Integer> nums = new ArrayList<>();
+        nums.add(7);
+        nums.add(2);
+        nums.add(3);
+        nums.add(4);
+        System.out.println(sum(nums));
 
     }
 }
