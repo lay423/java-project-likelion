ff8d20d383cd380f5dadc9520fc6d72ea4afdab5
lay423 <junpang486@gmail.com>
2022-11-23 09:15:30 +0900

    [Update] 피보나치수열 | 재귀함수

--- src/main/java/algorithm/week10/java1123/PrintStar.java
+++ src/main/java/algorithm/week10/java1123/PrintStar.java
@@ -1,5 +1,7 @@
 package algorithm.week10.java1123;
 
+import java.util.Scanner;
+
 public class PrintStar {
     public static void print(int n) {
         if(n==0)
@@ -8,7 +10,19 @@ public class PrintStar {
         print(n - 1);
     }
 
+    public static int fibonacci(int idx, int a, int b) {
+        if(idx <=2) return b;
+        int tmp = b;
+        b = a+b;
+        a = tmp;
+        return fibonacci(idx - 1, a, b);
+    }
+
     public static void main(String[] args) {
-        print(3);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fibonacci(n, 1, 1));
+
+
     }
 }
