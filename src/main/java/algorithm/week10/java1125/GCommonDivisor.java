595e2ecd7129e15a63bb9924256409192e620341
lay423 <junpang486@gmail.com>
2022-11-25 09:44:15 +0900

    [update] 최대공약수 | 재귀 없이

--- src/main/java/algorithm/week10/java1125/GCommonDivisor.java
+++ src/main/java/algorithm/week10/java1125/GCommonDivisor.java
@@ -4,31 +4,36 @@ import java.util.Scanner;
 
 public class GCommonDivisor {
     public static int gcd(int a, int b, int div) {
-        if(a%div ==0 && b%div==0)
+        if (a % div == 0 && b % div == 0)
             return div;
         return gcd(a, b, div - 1);
     }
 
     public static int gcd2(int a, int b) {
-        if(a>b)
-            return gcd2(a - b, b);
-        else if (a < b) {
-            return gcd2(a, b - a);
-        }else
-            return a;
+        if (a == b) return a;
+        else if (a < b) return gcd2(a, b - a);
+        else if (a > b) return gcd2(a - b, b);
+        else return 1;
+    }
+
+    public static int gcd3(int a, int b){
+        while (a != b) {
+            if (a > b) {
+                a = a-b;
+            }else {
+                b = b-a;
+            }
+        }
+        return a;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int div;
-        if (a > b) {
-            div=b;
-        }else {
-            div=a;
-        }
-        System.out.println(gcd(a, b, div));
+
+        System.out.println(gcd(a, b, Math.min(a, b)));
         System.out.println(gcd2(a, b));
+        System.out.println(gcd3(a,b));
     }
 }
