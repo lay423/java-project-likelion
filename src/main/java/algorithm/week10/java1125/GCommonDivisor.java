11f2131ab4923ac6e51b28767ab216f804427596
lay423 <junpang486@gmail.com>
2022-11-25 09:29:23 +0900

    [add] 최대공약수 | 파라미터 3개

---
+++ src/main/java/algorithm/week10/java1125/GCommonDivisor.java
@@ -0,0 +1,25 @@
+package algorithm.week10.java1125;
+
+import java.util.Scanner;
+
+public class GCommonDivisor {
+    public static int gcd(int a, int b, int div) {
+        if(a%div ==0 && b%div==0)
+            return div;
+        return gcd(a, b, div - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int div;
+        if (a > b) {
+            div=b;
+        }else {
+            div=a;
+        }
+        System.out.println(gcd(a, b, div));
+
+    }
+}
