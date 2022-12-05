34b210c574fb4b8f6c7ee6c536903a9b02e0b1ad
lay423 <junpang486@gmail.com>
2022-12-05 10:37:47 +0900

    [update] getChildrenIdx(), makeHeap() 추가

--- src/main/java/algorithm/week12/java1205/HeapSort.java
+++ src/main/java/algorithm/week12/java1205/HeapSort.java
@@ -1,20 +1,41 @@
 package algorithm.week12.java1205;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class HeapSort {
+    public int[] getChildrenIdx(int parentIdx) {
+        int lefIdx = parentIdx * 2 + 1;
+        int rightIdx = parentIdx * 2 + 2;
+        return new int[]{lefIdx, rightIdx};
+    }
+
+    public int[] makeHeap(int[] arr, int parentIdx) {
+        int leftIdx = parentIdx * 2 + 1;
+        int rightIdx = parentIdx * 2 + 2;
+        int greaterIdx = parentIdx;
+
+        if (leftIdx < arr.length && arr[leftIdx] > arr[greaterIdx]) {
+            greaterIdx = leftIdx;
+        }
+        return new int[]{};
+    }
+
     public static void main(String[] args) {
         int[] arr = {6, 5, 7, 8};
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] < arr[j]) {
-                    int tmp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = tmp;
-                }
-            }
-        }
+        int tmp;
+        tmp = arr[3];
+        arr[3] = arr[1];
+        arr[1] = tmp;
+
+        tmp = arr[0];
+        arr[0] = arr[1];
+        arr[1] = tmp;
         System.out.println(Arrays.toString(arr));
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
 
     }
 }
