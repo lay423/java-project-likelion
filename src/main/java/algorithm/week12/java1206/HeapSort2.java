ed196d4d65fd4b792ad7cc2701ad1a6a0479a0a4
lay423 <junpang486@gmail.com>
2022-12-06 10:44:10 +0900

    [update] 정렬에 loop 적용

--- src/main/java/algorithm/week12/java1206/HeapSort2.java
+++ src/main/java/algorithm/week12/java1206/HeapSort2.java
@@ -9,20 +9,24 @@ public class HeapSort2 {
         return new int[]{lefIdx, rightIdx};
     }
 
-    public static int[] makeHeap(int[] arr, int parentIdx) {
+    public static int[] makeHeap(int[] arr, int parentIdx, int arrSize) {
         int leftIdx = parentIdx * 2 + 1;
         int rightIdx = parentIdx * 2 + 2;
         int greaterIdx = parentIdx;
 
-        if (leftIdx < arr.length && arr[leftIdx] > arr[greaterIdx]) {
+        if (leftIdx < arrSize && arr[leftIdx] > arr[greaterIdx]) {
             greaterIdx = leftIdx;
+        }
+
+        if (rightIdx < arrSize && arr[rightIdx] > arr[greaterIdx]) {
+            greaterIdx = rightIdx;
         }
 
         if(parentIdx != greaterIdx){
             int tmp = arr[parentIdx];
             arr[parentIdx] = arr[greaterIdx];
             arr[greaterIdx] = tmp;
-            makeHeap(arr, greaterIdx);
+            makeHeap(arr, greaterIdx, arrSize);
         }
 
         return arr;
@@ -32,10 +36,17 @@ public class HeapSort2 {
         int[] arr = new int[]{5, 8, 4, 7, 3, 2, 9, 6, 7};
         arr = new int[]{10, 9, 5, 8, 3, 2, 4, 6, 7, 1};
         for (int i = (arr.length-2)/2; i >=0; i--) {
-            arr = makeHeap(arr, i);
+            arr = makeHeap(arr, i, arr.length);
             System.out.println(Arrays.toString(arr));
         }
 
+        for (int i = arr.length - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[arr.length-1] = temp;
+            arr = makeHeap(arr, 0, i);
+            System.out.println(Arrays.toString(arr));
 
+        }
     }
 }
