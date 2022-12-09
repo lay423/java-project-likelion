d18e4e2fcf31546b297d4b6b4b731395b6f11d1e
lay423 <junpang486@gmail.com>
2022-12-09 10:28:47 +0900

    [add] LCS | 최종코드

--- src/main/java/algorithm/week12/java1209/LCS.java
+++ src/main/java/algorithm/week12/java1209/LCS.java
@@ -1,35 +1,31 @@
 package algorithm.week12.java1209;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class LCS {
     public static void main(String[] args) {
-        String str1 = "ABCDCBA";
-        String str2 = "DCABDC";
-        int[][] arr = new int[str2.length()+1][str1.length()+1];
-        //Arrays.fill(arr, 0);
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.next();
+        String str2 = sc.next();
+
+        int[][] arr = new int[str2.length() + 1][str1.length() + 1];
 
         for (int i = 0; i < str2.length(); i++) {
             for (int j = 0; j < str1.length(); j++) {
-                int k = i+1;
-                int l = j+1;
-                System.out.printf("%s %s\n", str2.charAt(i), str1.charAt(j));
-                if(str2.charAt(i) == str1.charAt(j))
+                int k = i + 1;
+                int l = j + 1;
+                if (str2.charAt(i) == str1.charAt(j))
                     arr[k][l] = arr[k - 1][l - 1] + 1;
-                else{
+                else {
                     if (arr[k][l - 1] > arr[k - 1][l]) {
                         arr[k][l] = arr[k][l - 1];
-                    }else
-                        arr[k][l] = arr[k-1][l];
+                    } else
+                        arr[k][l] = arr[k - 1][l];
                 }
             }
         }
-        for (int i = 1; i <= str2.length(); i++) {
-            for (int j = 1; j <= str1.length(); j++) {
-                System.out.printf("%d ", arr[i][j]);
-
-            }
-            System.out.println();
-        }
+        System.out.println(arr[str2.length()][str1.length()]);
     }
 }
